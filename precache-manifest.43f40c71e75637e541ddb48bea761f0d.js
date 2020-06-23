c724aafd0a9e5ea0f6d30115497c391883f19b55
Tatiana-256 <tatiana.beznosiuk@gmail.com>
2020-06-23 09:05:13 +0300

    Updates

---
+++ precache-manifest.43f40c71e75637e541ddb48bea761f0d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cf29cfd8abe8c2f964bf0cb32ba319f3",
+    "url": "/cards/index.html"
+  },
+  {
+    "revision": "458bc040afafa111be51",
+    "url": "/cards/static/css/main.357e9a8c.chunk.css"
+  },
+  {
+    "revision": "d2d1a6341e3e57b665e7",
+    "url": "/cards/static/js/2.b60f14ab.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/cards/static/js/2.b60f14ab.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "458bc040afafa111be51",
+    "url": "/cards/static/js/main.14ddae9c.chunk.js"
+  },
+  {
+    "revision": "8e9c10436e990c8ebaef",
+    "url": "/cards/static/js/runtime-main.3640371d.js"
+  }
+]);
