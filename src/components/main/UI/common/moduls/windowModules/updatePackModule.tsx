2c0a7657d4fbd962ac2c52de872105ed3f353455
Tatiana-256 <tatiana.beznosiuk@gmail.com>
2020-07-17 18:10:16 +0300

    add handle for update

--- src/components/main/UI/common/moduls/windowModules/updatePackModule.tsx
+++ src/components/main/UI/common/moduls/windowModules/updatePackModule.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {ChangeEvent, useState} from 'react';
 import {Module} from "./UniversalModule";
 import CustomInput from "../../input/Input";
 import Button from "../../button/Button";
@@ -7,13 +7,21 @@ type PropsType = {
     showModule: boolean,
     packName: string,
     ChangePack: () => void
-    backgroundOnClick: ()=>void
+    backgroundOnClick: () => void
 }
 
 export const UpdatePackModule = (props: PropsType) => {
 
-    return <Module height={300} width={400} show={props.showModule} backgroundOnClick={props.backgroundOnClick} enableBackground={true}>
-        <CustomInput name={"Add new item"} value={props.packName}/>
+    const [packName, setPackName] = useState(props.packName)
+
+    const onchangeInput = (e: ChangeEvent<HTMLInputElement>) => {
+        setPackName(e.currentTarget.value)
+    }
+
+
+    return <Module height={300} width={400} show={props.showModule} backgroundOnClick={props.backgroundOnClick}
+                   enableBackground={true}>
+        <CustomInput name={"Add new item"} value={packName} onChange={onchangeInput}/>
         <Button buttonClass={'regularButton'} onClick={props.ChangePack}>Add</Button>
     </Module>
 }
