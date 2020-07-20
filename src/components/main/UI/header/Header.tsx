6ccdab619df61bf291fe0699f12d6b4795c9ba80
Tatiana-256 <tatiana.beznosiuk@gmail.com>
2020-07-20 17:58:08 +0300

    change view of header after logIn

--- src/components/main/UI/header/Header.tsx
+++ src/components/main/UI/header/Header.tsx
@@ -1,18 +1,25 @@
 import React from 'react';
 import styles from './Header.module.css';
 import {NavLink} from 'react-router-dom';
+import {useSelector} from "react-redux";
+import {AppStateType} from "../../BLL/redux-store";
 
-type PropsType = {}
+const Header = () => {
 
+    const {isSuccess} = useSelector((store: AppStateType) => store.login)
 
-const Header = (props: PropsType) => {
     return <div className={styles.header}>
         <NavLink to="/" className={styles.rout}> Profile </NavLink>
         <NavLink to="/cards/pack" className={styles.rout}> Cards </NavLink>
-        <NavLink to="/login" className={styles.rout}> Login </NavLink>
-        <NavLink to="/registration" className={styles.rout}> Registration </NavLink>
-        <NavLink to="/refreshPassword" className={styles.rout}> Remind password </NavLink>
-        <NavLink to="/set-new-password/:id" className={styles.rout}> New password </NavLink>
+        {isSuccess ?
+            <div className={styles.rout}> LogOut </div>
+            :
+            <>
+                <NavLink to="/login" className={styles.rout}> Login </NavLink>
+                <NavLink to="/registration" className={styles.rout}> Registration </NavLink>
+                <NavLink to="/refreshPassword" className={styles.rout}> Remind password </NavLink>
+                <NavLink to="/set-new-password/:id" className={styles.rout}> New password </NavLink>
+            </>}
     </div>
 }
 
